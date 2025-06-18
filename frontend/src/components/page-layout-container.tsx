2e0e8a516d9ff81eecaf96c61140525cc46bac97
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2025-06-18 11:17:39 +0300

    refactor: adjust padding in PageLayoutContainer for improved mobile layout
    - Added bottom padding to the main content area in PageLayoutContainer to enhance spacing on mobile devices.
    - Commented out the mobile-specific height div for better layout management while maintaining functionality.

--- frontend/src/components/page-layout-container.tsx
+++ frontend/src/components/page-layout-container.tsx
@@ -12,13 +12,13 @@ const PageLayoutContainer = ({ children }: PageLayoutContainerProps) => {
       <div className="fixed inset-0 bg-[#1A2027] flex items-center justify-center">
         <div className="relative w-full md:w-[26%] h-full md:h-[90%] md:rounded-[36px] bg-slate-100 flex flex-col">
           {/* Main Content Area with proper z-index */}
-          <div className="overflow-y-auto relative z-0 flex-1">
+          <div className="overflow-y-auto relative z-0 flex-1 pb-20 md:pb-0">
             <Header />
             {children}
           </div>
 
           {/* Fixed Bottom Navigation with higher z-index */}
-          <div className="w-full h-20 md:hidden"></div>
+          {/* <div className="w-full h-20 md:hidden"></div> */}
           <MainTabs />
         </div>
       </div>
